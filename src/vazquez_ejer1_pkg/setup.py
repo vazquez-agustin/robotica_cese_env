9e9c20a8302c1ca7bec80f26a1b0dd892d5a754a
vazquezagostino@gmail.com <vazquezagostino@gmail.com>
2025-06-05 01:30:37 +0100

    exercise1_package

---
+++ src/vazquez_ejer1_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'vazquez_ejer1_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'counter_publisher = vazquez_ejer1_pkg.counter_publisher:main',
+            'counter_subscriber = vazquez_ejer1_pkg.counter_subscriber:main',
+        ],
+    },
+)
